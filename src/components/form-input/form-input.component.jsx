fb3e55c1a674e042f9efc9e466c1768d9f1e7302
Niharika Vyas <nvyas145@gmail.com>
2025-03-24 12:17:57 +0530

    add-ons

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,19 +1,19 @@
-import "./form-input.style.scss";
+import { FormInputLabel, Input, Group } from "./form-input.style.jsx";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label
+        <FormInputLabel
           className={`form-input-label ${
             otherProps.value.length ? "shrink" : ""
           }`}
         >
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 export default FormInput;
--- src/components/sign-in-form/sign-in.component.jsx
+++ src/components/sign-in-form/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
@@ -78,7 +80,11 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button
+            type={BUTTON_TYPE_CLASSES.google}
+            buttonType="google"
+            onClick={signInWithGoogle}
+          >
             Google sign In
           </Button>
         </div>
